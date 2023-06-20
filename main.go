b12b3a22ea5b9045a67758e9844ab18f76218082
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-06-20 16:51:31 +0100

    Show and add quotes

--- main.go
+++ main.go
@@ -6,15 +6,40 @@ import (
 	"net/http"
 )
 
+type Quote struct {
+	Quote         string
+	Book          string
+	DateSubmitted string
+}
+
+var quotes = []Quote{
+	{Quote: "Hello World", Book: "Some Book", DateSubmitted: "now"},
+}
+
 func main() {
 	// Hello world, the web server
 
 	println("Staring server on port 8080")
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/add-quote/", add_quote)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func helloHandler(w http.ResponseWriter, req *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
+	quotes := map[string][]Quote{
+		"Quotes": quotes,
+	}
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, quotes)
+}
+
+func add_quote(w http.ResponseWriter, r *http.Request) {
+	quote := r.PostFormValue("quote")
+	book := r.PostFormValue("book")
+	date_submitted := "now"
+	quote_record := Quote{Quote: quote, Book: book, DateSubmitted: date_submitted}
+	quotes = append(quotes, quote_record)
+
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl.ExecuteTemplate(w, "quote", quote_record)
 }
