88de8639644ebedd560c4509d2bfd89138905959
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-06-20 18:45:52 +0100

    Switch to using pocketbase

--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"sort"
 	"time"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
 )
+
+// Boilerplate from https://echo.labstack.com/guide/templates/
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 type Quote struct {
 	Quote         string
@@ -19,13 +33,27 @@ var quotes = []Quote{
 }
 
 func main() {
-	println("Staring server on port 8080")
-	http.HandleFunc("/", index)
-	http.HandleFunc("/add-quote/", add_quote)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	app := pocketbase.New()
+
+	// Pre-compile templates
+	t := &Template{
+		templates: template.Must(template.ParseGlob("templates/*.html")),
+	}
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.Renderer = t
+		e.Router.GET("/", index)
+		e.Router.POST("/add-quote/", add_quote)
+
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
+func index(c echo.Context) error {
 	sorted_quotes := quotes
 	sort.Slice(sorted_quotes, func(i, j int) bool {
 		return sorted_quotes[i].DateSubmitted > sorted_quotes[j].DateSubmitted
@@ -34,17 +62,15 @@ func index(w http.ResponseWriter, r *http.Request) {
 	tmpl_args := map[string][]Quote{
 		"Quotes": sorted_quotes,
 	}
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	tmpl.Execute(w, tmpl_args)
+	return c.Render(http.StatusOK, "index.html", tmpl_args)
 }
 
-func add_quote(w http.ResponseWriter, r *http.Request) {
-	quote := r.PostFormValue("quote")
-	book := r.PostFormValue("book")
+func add_quote(c echo.Context) error {
+	quote := c.FormValue("quote")
+	book := c.FormValue("book")
 	date_submitted := time.Now().UTC().String()
 	quote_record := Quote{Quote: quote, Book: book, DateSubmitted: date_submitted}
 	quotes = append(quotes, quote_record)
 
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	tmpl.ExecuteTemplate(w, "quote", quote_record)
+	return c.Render(http.StatusOK, "quote", quote_record)
 }
