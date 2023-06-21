403d44166636bb83ef7225d5249e9f223457288d
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-06-21 13:12:35 +0100

    Revert "Experimenting with fiber"
    This reverts commit 69ebb95969f113cc833f2547779d22c4beb62e41.

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require github.com/pocketbase/pocketbase v0.16.5
 
 require (
 	github.com/AlecAivazis/survey/v2 v2.3.6 // indirect
-	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/aws/aws-sdk-go v1.44.274 // indirect
 	github.com/aws/aws-sdk-go-v2 v1.18.0 // indirect
@@ -35,10 +34,6 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/ganigeorgiev/fexpr v0.3.0 // indirect
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
-	github.com/gofiber/fiber/v2 v2.47.0 // indirect
-	github.com/gofiber/template v1.8.2 // indirect
-	github.com/gofiber/template/html/v2 v2.0.4 // indirect
-	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
@@ -48,27 +43,18 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
-	github.com/klauspost/compress v1.16.3 // indirect
 	github.com/labstack/echo/v5 v5.0.0-20220201181537-ed2888cfa198 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mattn/go-sqlite3 v1.14.17 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
-	github.com/philhofer/fwd v1.1.2 // indirect
 	github.com/pocketbase/dbx v1.10.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
-	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/tinylib/msgp v1.1.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.47.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	gocloud.dev v0.29.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
@@ -76,7 +62,7 @@ require (
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/oauth2 v0.8.0 // indirect
-	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/term v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"log"
+	"net/http"
+	"sort"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html/v2"
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/models"
 )
+
+// Boilerplate from https://echo.labstack.com/guide/templates/
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 type Quote struct {
 	Text          string
@@ -13,49 +28,73 @@ type Quote struct {
 	DateSubmitted string
 }
 
+var app *pocketbase.PocketBase
+
 func main() {
-	engine := html.New("./views", ".html")
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
-	app.Get("/", index)
-	app.Post("/add-quote/", addQuote)
+	app = pocketbase.New()
 
-	log.Fatal(app.Listen(":3000"))
-}
-
-func index(c *fiber.Ctx) error {
-	// quotes := []Quote{}
-	// for _, record := range records {
-	// 	quotes = append(quotes, new_quote(record))
-	// }
-	// sort.Slice(quotes, func(i, j int) bool {
-	// 	return quotes[i].DateSubmitted > quotes[j].DateSubmitted
-	// })
-
-	quotes := []Quote{
-		{
-			Text:          "This is a quote",
-			Book:          "This is a book",
-			DateSubmitted: "This is a date",
-		},
+	// Pre-compile templates
+	t := &Template{
+		templates: template.Must(template.ParseGlob("templates/*.html")),
 	}
-	return c.Render("index", fiber.Map{
-		"Quotes": quotes,
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.Renderer = t
+		e.Router.GET("/", index)
+		e.Router.POST("/add-quote/", add_quote)
+
+		return nil
 	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
 
-func addQuote(c *fiber.Ctx) error {
+func index(c echo.Context) error {
+	records, err := app.Dao().FindRecordsByExpr("quotes")
+	if err != nil {
+		return c.Render(http.StatusInternalServerError, "error.html", err)
+	}
+
+	quotes := []Quote{}
+	for _, record := range records {
+		quotes = append(quotes, new_quote(record))
+	}
+	sort.Slice(quotes, func(i, j int) bool {
+		return quotes[i].DateSubmitted > quotes[j].DateSubmitted
+	})
+
+	tmpl_args := map[string][]Quote{
+		"Quotes": quotes,
+	}
+	return c.Render(http.StatusOK, "index.html", tmpl_args)
+}
+
+func add_quote(c echo.Context) error {
 	text := c.FormValue("text")
 	book := c.FormValue("book")
-	quote := Quote{
-		Text:          text,
-		Book:          book,
-		DateSubmitted: "Today",
-	}
-	log.Println("Added quote: ", quote)
 
-	return c.Render("quote", fiber.Map{
-		"Quote": quote,
-	})
+	collection, err := app.Dao().FindCollectionByNameOrId("quotes")
+	if err != nil {
+		return c.Render(http.StatusInternalServerError, "error.html", err)
+	}
+
+	record := models.NewRecord(collection)
+	record.Set("text", text)
+	record.Set("book", book)
+
+	if err := app.Dao().SaveRecord(record); err != nil {
+		return c.Render(http.StatusInternalServerError, "error.html", err)
+	}
+
+	return c.Render(http.StatusOK, "quote", new_quote(record))
+}
+
+func new_quote(record *models.Record) Quote {
+	return Quote{
+		Text:          record.GetString("text"),
+		Book:          record.GetString("book"),
+		DateSubmitted: record.GetString("created"),
+	}
 }
