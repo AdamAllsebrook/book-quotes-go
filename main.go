69ebb95969f113cc833f2547779d22c4beb62e41
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-06-21 13:02:29 +0100

    Experimenting with fiber

--- main.go
+++ main.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	"html/template"
-	"io"
 	"log"
-	"net/http"
-	"sort"
 
-	"github.com/labstack/echo/v5"
-	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/core"
-	"github.com/pocketbase/pocketbase/models"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
-
-// Boilerplate from https://echo.labstack.com/guide/templates/
-type Template struct {
-	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
 
 type Quote struct {
 	Text          string
@@ -28,73 +13,49 @@ type Quote struct {
 	DateSubmitted string
 }
 
-var app *pocketbase.PocketBase
-
 func main() {
-	app = pocketbase.New()
-
-	// Pre-compile templates
-	t := &Template{
-		templates: template.Must(template.ParseGlob("templates/*.html")),
-	}
-
-	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		e.Router.Renderer = t
-		e.Router.GET("/", index)
-		e.Router.POST("/add-quote/", add_quote)
-
-		return nil
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
 	})
+	app.Get("/", index)
+	app.Post("/add-quote/", addQuote)
 
-	if err := app.Start(); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(app.Listen(":3000"))
 }
 
-func index(c echo.Context) error {
-	records, err := app.Dao().FindRecordsByExpr("quotes")
-	if err != nil {
-		return c.Render(http.StatusInternalServerError, "error.html", err)
-	}
+func index(c *fiber.Ctx) error {
+	// quotes := []Quote{}
+	// for _, record := range records {
+	// 	quotes = append(quotes, new_quote(record))
+	// }
+	// sort.Slice(quotes, func(i, j int) bool {
+	// 	return quotes[i].DateSubmitted > quotes[j].DateSubmitted
+	// })
 
-	quotes := []Quote{}
-	for _, record := range records {
-		quotes = append(quotes, new_quote(record))
+	quotes := []Quote{
+		{
+			Text:          "This is a quote",
+			Book:          "This is a book",
+			DateSubmitted: "This is a date",
+		},
 	}
-	sort.Slice(quotes, func(i, j int) bool {
-		return quotes[i].DateSubmitted > quotes[j].DateSubmitted
-	})
-
-	tmpl_args := map[string][]Quote{
+	return c.Render("index", fiber.Map{
 		"Quotes": quotes,
-	}
-	return c.Render(http.StatusOK, "index.html", tmpl_args)
+	})
 }
 
-func add_quote(c echo.Context) error {
+func addQuote(c *fiber.Ctx) error {
 	text := c.FormValue("text")
 	book := c.FormValue("book")
-
-	collection, err := app.Dao().FindCollectionByNameOrId("quotes")
-	if err != nil {
-		return c.Render(http.StatusInternalServerError, "error.html", err)
+	quote := Quote{
+		Text:          text,
+		Book:          book,
+		DateSubmitted: "Today",
 	}
+	log.Println("Added quote: ", quote)
 
-	record := models.NewRecord(collection)
-	record.Set("text", text)
-	record.Set("book", book)
-
-	if err := app.Dao().SaveRecord(record); err != nil {
-		return c.Render(http.StatusInternalServerError, "error.html", err)
-	}
-
-	return c.Render(http.StatusOK, "quote", new_quote(record))
-}
-
-func new_quote(record *models.Record) Quote {
-	return Quote{
-		Text:          record.GetString("text"),
-		Book:          record.GetString("book"),
-		DateSubmitted: record.GetString("created"),
-	}
+	return c.Render("quote", fiber.Map{
+		"Quote": quote,
+	})
 }
